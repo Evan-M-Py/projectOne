533b4c7736668993570ea6591f7a268a1052b03c
blinkjuan82 <ceratio24@gmail.com>
2020-01-11 11:51:50 -0700

    quote generator working and and music finder working

--- assets/script.js
+++ assets/script.js
@@ -22,41 +22,40 @@
         astroFunction()
     });
     
-    //Quote Generator function
-    function quoteGen() {
-    $(document).ready(function(){
-        var quote;
-        var author;
-        function getNewQuote(){
-            $.ajax({
-                url:'http://api.forismatic.com/api/1.0/',
-                jsonp: 'jsonp',
-                dataType: 'jsonp',
-                data: {
-                    method: 'getQuote',
-                    lang: 'en',
-                    format:'jsonp'
-                },
-                success: function(response){
-                    quote = response.quoteText;
-                    author = response.quoteAuthor;
-                    $('#quote').text(quote);
-                    if (author){
-                        $('#author').text('-' + author);
-                    }
-                    else{
-                        $('#author').text('-Anonymous');
-                    }
+  //random quote function
+$(document).ready(function(){
+    var quote;
+    var author;
+
+    function getNewQuote(){
+        $.ajax({
+            url:'http://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp',
+            dataType: 'jsonp',
+            data: {
+                method: 'getQuote',
+                lang: 'en',
+                format:'jsonp'
+            },
+            success: function(response){
+                quote = response.quoteText;
+                author = response.quoteAuthor;
+                $('#quote').text(quote);
+                if (author){
+                    $('#author').text('-' + author);
                 }
-            });
-        }
-        getNewQuote();
-        $(".get-quote").on("click", function(event){
-            event.preventDefault();
-            getNewQuote();
+                else{
+                    $('#author').text('-Anonymous');
+                }
+            }
         });
+    }
+    getNewQuote();
+    $(".get-quote").on("click", function(event){
+        event.preventDefault();
+        getNewQuote();
     });
-    };
+});
     
     //Astrological Function
     function astroFunction (){ 
@@ -78,3 +77,67 @@
             }
              })};
     
+    //Song Finder
+let term = ''
+const SongContainer = document.getElementById('songs')
+
+
+const updateTerm = () => {
+    term = document.getElementById('searchInput').value;
+    if (!term || term === '') {
+        alert('You entered nothing!');
+    } else {
+        while(SongContainer.firstChild){
+            SongContainer.removeChild(SongContainer.firstChild);
+        }
+        const url = `https://itunes.apple.com/search?limit=1&media=music&term=${term}`
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                // console.log(data.results);
+                const artists = data.results;
+                return artists.map(result => {
+
+                    const article = document.createElement('article'),
+                        artist = document.createElement('p'),
+                        song = document.createElement('p'),
+                        img = document.createElement('img'),
+                        audio = document.createElement('audio'),
+                        audioSource = document.createElement('source');
+                        
+                
+                        
+
+                    artist.innerHTML = result.artistName
+                    song.innerHTML = result.trackName
+                    img.src = result.artworkUrl100
+                    audioSource.src = result.previewUrl
+                    audio.setAttribute('controls', '')
+                
+                    
+
+                    article.appendChild(img)
+                    article.appendChild(artist)
+                    article.appendChild(song)
+                    article.appendChild(audio)
+                    audio.appendChild(audioSource)
+                    SongContainer.appendChild(article)
+                    
+                  
+                })
+            })
+            .catch(error => console.log('Request Failed: ', error))
+    }
+}
+
+const searchBtn = document.getElementById('songSearch');
+searchBtn.addEventListener('click', updateTerm);
+document.addEventListener('play', event=>{
+    const audio = document.getElementsByTagName('audio');
+    for(let i = 0; i<audio.length; i++){
+        if(audio[i] != event.target){
+            audio[i].pause();
+            console.log(event);
+        }
+    }
+}, true)
